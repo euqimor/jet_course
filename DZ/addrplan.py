47040b0e4e40d5a43f39a36b1f90985133764a3a
euqimor <romique@gmail.com>
2017-08-19 04:04:11 +0300

    DZ progress commit

--- DZ/addrplan.py
+++ DZ/addrplan.py
@@ -1,22 +1,13 @@
-import glob
-from re import search
+import glob, itertools, sys, time
+from re import findall
 
 def getAddr(str):
-    expIntAddr('^ *interface +(.+) *\n *ip address (([0-9]\.?)*) +(([0-9]\.?)*)')
-    expIP = '^ *ip +address +(([0-9]\.?)+) +(([0-9]\.?)+)'
-    expInt = '^ *interface +(.+)'
-    expHost = '^ *hostname +(.+)'
-    if bool(search(expIP,str)):
-        result = search(expIP,str)
-        return ('IP',result.group(1),result.group(3))
-    elif bool(search(expInt,str)):
-        result = search(expInt,str)
-        return ('INT',result.group(1))
-    elif bool(search(expHost,str)):
-        result = search(expHost,str)
-        return ('HOST',result.group(1))
+    expAddr = ('interface +(.+) *\n(.*\n(?!!))* *ip address (([0-9]\.?)*) +(([0-9]\.?)*)')
+    result = findall(expAddr,str)
+    if result:
+        return result
     else:
-        return ('UNCLASSIFIED',)
+        return False
 
 def getPath():
     path = 'C:\\python_local\\projects\\p4ne\Lab1.5\\config_files\\'
@@ -24,40 +15,60 @@ def getPath():
     tmp = input('Press Enter to use the default filepath or enter the path manually:')
     if tmp != '':
         path = tmp
-    print('\nWorking, please wait...')
     return path
 
-path = getPath()
+def spinAnimation():
+    spinner = itertools.cycle(['/','-','\\','|'])
+    print(next(spinner),end='')
+    time.sleep(0.2)
 
-lTxt = []
-while len(lTxt) == 0:
-    lTxt = glob.glob(path+'*.txt')
-    if len(lTxt) == 0:
-        print('\nNo txt files found under the specified path.')
-        path = getPath()
+def spinTest():
+    for i in range(100):
+        spinAnimation()
 
-strList = []
-for filePath in lTxt:
-    file = open(filePath)
-    for string in file:
-        tmp = strClass(string)
-        if tmp[0] != 'UNCLASSIFIED':
-            strList.append(tmp)
-    file.close()
+
+def getFileList(path):
+    fileList = []
+    while len(fileList) == 0:
+        fileList = glob.glob(path + '*.txt')
+        if len(fileList) == 0:
+            print('\nNo txt files found under the specified path.')
+            path = getPath()
+    return fileList
+
+def getAddressList(fileList):
+    addressList = []
+    print('\nWorking, please wait...')
+    for filePath in fileList:
+        with open(filePath) as file:
+            data = file.read()
+            addresses = getAddr(data)
+            if addresses:
+                for item in addresses:
+                    spinAnimation()
+                    addressList.append(item)
+    return addressList
 
 def areYouSure():
-    print('\nThe resulting list is '+str(len(strList))+' long.')
-    answer = input('Are you sure you want to print it? [Y/N]')
+    path = getPath()
+    fileList = getFileList(path)
+    addressList = getAddressList(fileList)
+    print('\nThe resulting list is ' + str(len(addressList)) + ' long.')
+    answer = input('Are you sure you want to print it?[Y]') or 'Y'
     while True:
         if answer == 'Y' or answer == 'y':
+            i = 0
             print('\n')
-            for entry in strList:
-                print(entry)
+            for entry in addressList:
+                i+=1
+                print('|{: 4}| {: <15} {: <15}|'.format(i,entry[2],entry[4]))
+                print('|'+'_'*4+'|'+'_'*32+'|')
             break
         elif answer == 'N' or answer == 'n':
             print('\nWise choice!')
             break
         else:
-            answer = input('Please answer Y or N:')
+            answer = input('Please answer Y or N [Y]:') or 'Y'
 
-areYouSure()
+spinTest()
+# areYouSure()
